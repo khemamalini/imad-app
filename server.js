916525b9f3b9052a2b5943e7c4a0f199ca05a96c
khemamalini <30633085+khemamalini@users.noreply.github.com>
2017-08-16 13:48:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('articleone', function (req, res) {
+  res.sendFile('Article one requested here and will be served');
+});
+app.get('articletwo', function (req, res) {
+  res.sendFile('Article two requested here and will be served');
+});
+app.get('articlethree', function (req, res) {
+  res.sendFile('Article three requested here and will be served');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
