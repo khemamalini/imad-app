3a73b8d1e89f89afef4e205d28c4834a46483891
Hemamalini <30633085+khemamalini@users.noreply.github.com>
2017-08-17 22:50:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get('/article-three', function (req, res) {
   res.send('Article three requested here and will be served');
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
